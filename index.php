eaed7201d53b7b7efbde53d3fe7396d7dea876d3
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-06-27 15:43:17 +0200

    exo completed

--- index.php
+++ index.php
@@ -1,5 +1,54 @@
 <?php
-$name = 'prenom';
-echo $name;
+
+//exo1
+// $name = 'prenom';
+// echo $name;
+
+//exo 2
+// $name = 'name';
+// $lastname = 'lastname';
+// $age = 21;
+
+// echo "$name , $lastname , $age";
+
+//exo3
+// $km = 1;
+// $km = $km*2;
+// echo "$km ";
+// $km = 125;
+// echo "$km";
+
+//exo4
+// $data = array(1, 1.12342, true, 'foo');
+
+// foreach ($data as $value) {
+//     echo gettype($value), "\n";
+// }
+
+//exo5
+// $number = 0;
+// echo ("$number<br>");
+// if (empty($number)) {
+//     echo "Variable 'number' is empty.<br>";
+//   }
+
+//exo6
+// $name = 'name';
+// echo ("Bonjour ".$name.", comment vas tu ?");
+
+// exo7
+// $name  = 'name';
+// $firstname = 'firsname';
+// $age = 23;
+// echo ("bonjour $name $firstname tu a $age ans "); 
+
+//exo 8
+$first = 3+4;
+$second = 5*20;
+$third = 45/5;
+echo ("$first $second $third " )
+
+
+
 
 ?>
